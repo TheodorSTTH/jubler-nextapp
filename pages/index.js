44cd9092ec78a7c1ba97b85edb720c0a01252719
Theodor <95082046+TheodorSTTH@users.noreply.github.com>
2022-12-07 10:46:54 +0100

    API connection test

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,10 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  async function apiTest() {
+    const res = await fetch("https://p1ojmprnji.execute-api.eu-north-1.amazonaws.com/TestStage/my-test");
+    console.log(res);
+  }
   return (
     <div>
       <Head>
@@ -12,6 +16,9 @@ export default function Home() {
       </Head>
       <h1 className="text-3xl font-bold underline">
         Hello world! I love tailwindcss
+        <button onClick={apiTest}>
+          API CALL
+        </button>
       </h1>
     </div>
   )
